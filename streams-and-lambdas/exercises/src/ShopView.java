66aa4b07c4df7b981c3f36872fab2f2a7423c9db
Tadas Obolevicius <tobolevicius@gmail.com>
2019-02-05 15:15:06 +0200

    Create ShopView.java

---
+++ streams-and-lambdas/exercises/src/ShopView.java
@@ -0,0 +1,18 @@
+public class ShopView {
+
+    private int id;
+    private String name;
+
+    public ShopView(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
