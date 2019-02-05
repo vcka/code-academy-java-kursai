4d979d5eed76339a41c090d97cbdd7e67ec77df5
Tadas Obolevicius <tobolevicius@gmail.com>
2019-02-05 22:01:22 +0200

    Create ExerciseMain.java

---
+++ streams-and-lambdas/exercises/src/ExerciseMain.java
@@ -0,0 +1,28 @@
+package exercise;
+
+import java.util.Optional;
+
+public class ExerciseMain {
+
+    public static void main(String[] args) {
+        ShopService service = new DefaultShopService();
+
+        print(service.getAllShops());
+
+        System.out.println(service
+                .getShopById(1,
+                        () -> new ShopView(1, "DefaultShop")));
+
+        System.out.println(service
+                .getShopById(98,
+                        () -> new ShopView(98, "DefaultShop")));
+    }
+
+    private static void print(Iterable iterable) {
+        for (Object object : iterable) {
+            System.out.println(object);
+
+        }
+        System.out.println("-----------------------------");
+    }
+}
