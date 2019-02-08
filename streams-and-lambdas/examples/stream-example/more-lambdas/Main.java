f18d3163447002453721c8a8ed41c54e04d9e2df
Tadas Obolevicius <tobolevicius@gmail.com>
2019-02-08 18:06:57 +0200

    Create Main.java

---
+++ streams-and-lambdas/examples/stream-example/more-lambdas/Main.java
@@ -0,0 +1,25 @@
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import static java.util.Collections.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+
+
+    private static List<Message> messages(int n) {
+        List<Message> messages = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            messages
+                    .add(new Message(i, i + " " + i * 10,
+                            LocalDateTime.now(), MessageType.TEXT_MESSAGE,
+                            emptyList()));
+        }
+        return messages;
+    }
+}
